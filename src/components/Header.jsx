cd8760a4be73088ec7fc6a83393a7a477778ad37
ayrtoncdev <ayrtonc.dev@gmail.com>
2024-05-21 17:49:12 -0300

    Agregado pequeño e inicial de estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { NavBar } from './NavBar'
 
 export const Header = () => {
   return (
-    <header className='bg-red-500'>
-        <img src={logo} alt="Logo" />
+    <header className='bg-red-500 h-20 flex place-content-between p-2 items-center'>
+        <img className='h-20' src={logo} alt="Logo" />
         <NavBar/>
     </header>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import burguerMenu from '../assets/icon/menu-hamburguesa.svg'
 
 export const NavBar = () => {
   return (
-    <ul className='bg-green-400 hidden'>
+    <>
+      <ul className='
+        bg-green-400 hidden
+        sm:flex sm:w-[438px] sm:place-content-evenly sm:text-base sm:items-center
+        md:w-[800px] md:text-lg 
+        '>
         <li>Institucional</li>
         <li>Infraestructura</li>
         <li>Carreras</li>
         <li>Académica</li>
         <li>Estudiantes</li>
-    </ul>
+      </ul>
+      <img className="w-10 h-8 sm:hidden" src={burguerMenu} alt="Menu Hamburguesa" />
+    </>
   )
 }
